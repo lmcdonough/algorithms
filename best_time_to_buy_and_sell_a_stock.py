7ed0e3138b2e422ee454e05daa0867212759a8d5
Levi McDonough <levi@govividly.com>
2023-08-03 17:01:32 -0400

    incorrectly added codespaces to gitignore but didnt work TODO.

--- best_time_to_buy_and_sell_a_stock.py
+++ best_time_to_buy_and_sell_a_stock.py
@@ -11,6 +11,7 @@ class Solution:
             else:
                 temp_profit = prices[r] - prices[l]
                 max_profit = max(max_profit, temp_profit)
+                # trunk-ignore(ruff/E741)
                 l = r
                 r+=1
         return max_profit
@@ -18,6 +19,7 @@ class Solution:
 
 def test_max_profit():
     s = Solution()
+    # trunk-ignore(bandit/B101)
     assert s.maxProfit([7,1,5,3,6,4]) == 5
     assert s.maxProfit([7,6,4,3,1]) == 0
     assert s.maxProfit([1,2,3,4,5]) == 4
